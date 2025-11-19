d36dd33c21b208efcfbf09162092a49f1080931c
MyFlyChicken <88094854+MyFlyChicken@users.noreply.github.com>
2025-11-19 10:46:58 +0800

    编写测试读取内容ceedling用例

--- mcu_sensor_test/test/test_sensor.c
+++ mcu_sensor_test/test/test_sensor.c
@@ -8,37 +8,48 @@
 #include "sensor.h"
 #include <stdio.h>
 void setUp(void) { /*!< 测试环境初始化 */
-  printf(""
-         "Test Start...\n");
 }
 
 void tearDown(void) { /*!< 测试环境去初始化 */
-  printf("Test End...\n");
 }
 
-void test_sensor_NeedToImplement(void) {
-
+void test_sensor_init(void) {
   int ret = 0;
-  uint8_t value[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  uint8_t read_buffer[10] = {0};
-
-  printf("__function__=%s, __LINE__=%d\n", __FUNCTION__, __LINE__);
-
-  // bsp_i2c_read_ExpectWithArrayAndReturn(0x50, 0x01, value, 10, 0);
-  bsp_i2c_read_ExpectAndReturn(0x50, 0x01, read_buffer, 10, 0);
-  ret = sensor_read_data(0x50, 0x01, read_buffer, 10);
-  TEST_ASSERT_EQUAL(0, ret);
-  TEST_ASSERT_EQUAL_UINT8_ARRAY(read_buffer, read_buffer, 10);
-}
-
-void test_sensor_init_NeedToImplement(void) {
-  int ret = 0;
-
-  printf("__function__=%s, __LINE__=%d\n", __FUNCTION__, __LINE__);
 
   bsp_i2c_init_ExpectAndReturn(0);
   ret = sensor_init();
   TEST_ASSERT_EQUAL(0, ret);
+}
+
+void test_sensor_read_data_ret(void) {
+  int ret = 0;
+  uint8_t read_buffer[5] = {0};
+
+  bsp_i2c_read_ExpectAndReturn(0x60, 0x02, read_buffer, 5, 0);
+  ret = sensor_read_data(0x60, 0x02, read_buffer, 5);
+  TEST_ASSERT_EQUAL(0, ret);
+}
+
+/**
+ * @brief 测试读取内容是否符合预期
+ *
+ * @details 步骤1：编辑yml，使能cmock插件array和return_thru_ptr
+ *          步骤2：设置bsp_i2c_read被调用时，返回值为0
+ *          步骤2：设置bsp_i2c_read被调用时，将expected_buffer的数据赋值给实参data指向的内存
+ */
+void test_sensor_read_data_content(void) {
+  int ret = 0;
+  uint8_t read_buffer[5] = {0};
+  uint8_t expected_buffer[5] = {10, 20, 30, 40, 50};
+
+  // 设置bsp_i2c_read被调用时，返回值为0
+  bsp_i2c_read_ExpectAndReturn(0x60, 0x02, read_buffer, 5, 0);
+  // 设置bsp_i2c_read被调用时，将expected_buffer的数据赋值给实参data指向的内存
+  bsp_i2c_read_ReturnArrayThruPtr_data(expected_buffer, 5);
+
+  ret = sensor_read_data(0x60, 0x02, read_buffer, 5);
+  TEST_ASSERT_EQUAL(0, ret);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_buffer, read_buffer, 5);
 }
 
 #endif // TEST
